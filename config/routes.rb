47090db48f7635fd775c522158a0068828bd107c
yasir101 <yasir.amjad101@gmail.com>
2020-08-18 17:02:22 +0500

    all project done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'messages/create'
   root 'chatroom#index'
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  post 'message', to: 'messages#create'
+
+  mount ActionCable.server , at: '/cable'
 end
